2ca269a8a9392a674c419e2d1fd730496c4fa71d
dagci <dagci@TR-ADN-13SZ633>
2022-06-14 16:19:24 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+MyClass myClass = new();
+var sum = myClass.Sum(3,5);
+Console.WriteLine(sum);
+class MyClass {
+    public int Sum(int firstNumber,int secondNumber) { 
+        return firstNumber + secondNumber;
+    }
+}
